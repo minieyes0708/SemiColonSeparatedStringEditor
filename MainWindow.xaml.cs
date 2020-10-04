20f968867d69b65307d06f195ac0535108810684
minieyes <chenvey2@gmail.com>
2020-10-04 18:23:32 +0800

    now can drag to change position

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -39,6 +40,24 @@ namespace SemiColonSeparatedStringEditor
         {
             data_model.SeparatedList.Add(new ItemModel());
             scrSeparatedList.ScrollToEnd();
+        }
+        private void gridPathItem_Drop(object sender, DragEventArgs e)
+        {
+            Grid grid = sender as Grid;
+            ItemModel move_target = grid.Tag as ItemModel;
+            int insert_index = data_model.SeparatedList.IndexOf(move_target);
+            if (e.Data.GetDataPresent(typeof(ItemModel)))
+            {
+                ItemModel to_be_moved = e.Data.GetData(typeof(ItemModel)) as ItemModel;
+                data_model.SeparatedList.Remove(to_be_moved);
+                data_model.SeparatedList.Insert(insert_index, to_be_moved);
+            }
+        }
+        private void btnDrag_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Button btn = sender as Button;
+            ItemModel item = btn.Tag as ItemModel;
+            DragDrop.DoDragDrop(btn, item, DragDropEffects.Move);
         }
     }
 }
