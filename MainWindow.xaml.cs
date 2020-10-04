d11b709b6d60bacc0b404d331e85f973b67eaf08
minieyes <chenvey2@gmail.com>
2020-10-04 18:31:37 +0800

    add Drag/Drop Indicative UI response

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -41,10 +41,27 @@ namespace SemiColonSeparatedStringEditor
             data_model.SeparatedList.Add(new ItemModel());
             scrSeparatedList.ScrollToEnd();
         }
-        private void gridPathItem_Drop(object sender, DragEventArgs e)
+        private void btnDrag_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
-            Grid grid = sender as Grid;
-            ItemModel move_target = grid.Tag as ItemModel;
+            Button btn = sender as Button;
+            ItemModel item = btn.Tag as ItemModel;
+            DragDrop.DoDragDrop(btn, item, DragDropEffects.Move);
+        }
+        private void btnDrag_DragEnter(object sender, DragEventArgs e)
+        {
+            Button btn = sender as Button;
+            btn.BorderBrush = Brushes.Purple;
+        }
+        private void btnDrag_DragLeave(object sender, DragEventArgs e)
+        {
+            Button btn = sender as Button;
+            btn.BorderBrush = Brushes.Transparent;
+        }
+        private void btnDrag_Drop(object sender, DragEventArgs e)
+        {
+            Button btn = sender as Button;
+            btn.BorderBrush = Brushes.Transparent;
+            ItemModel move_target = btn.Tag as ItemModel;
             int insert_index = data_model.SeparatedList.IndexOf(move_target);
             if (e.Data.GetDataPresent(typeof(ItemModel)))
             {
@@ -52,12 +69,6 @@ namespace SemiColonSeparatedStringEditor
                 data_model.SeparatedList.Remove(to_be_moved);
                 data_model.SeparatedList.Insert(insert_index, to_be_moved);
             }
-        }
-        private void btnDrag_PreviewMouseDown(object sender, MouseButtonEventArgs e)
-        {
-            Button btn = sender as Button;
-            ItemModel item = btn.Tag as ItemModel;
-            DragDrop.DoDragDrop(btn, item, DragDropEffects.Move);
         }
     }
 }
